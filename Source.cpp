eb91715602e0a1f73dc85e4f9f25488b70f81496
cppandria <vadim.priy@gmail.com>
2023-05-28 17:09:06 +0300

    first commit

--- Source.cpp
+++ Source.cpp
@@ -1,29 +1,62 @@
+
+#include <string>
 #include <iostream>
-#include <fstream>
-#include <sstream>
+#include <iomanip>
+#include <ctime>
+#include <random>
+
 using namespace std;
 
-string get_string_from_file(string file_name) {
-	ofstream myfile;
-	std::ifstream file(file_name);
-
-	std::stringstream buffer;
-	if (file)
+void fill_array_with_randoms(int **arr, int rows, int colomn, int min_range, int max_range) {
+	int range = max_range - min_range + 1;
+	for (int i = 0; i < rows; i++)
 	{
-		buffer << file.rdbuf();
-		file.close();
+		for (int j = 0; j < colomn; j++)
+		{
+			arr[i][j] = rand() % range + min_range;
+		}
 	}
-	return buffer.str();
+}
+void print_arr(int **arr, int rows, int colomn) {
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < colomn; j++)
+		{
+			cout << setw(3) << arr[i][j] << " ";
+		}
+		cout << endl;
+	}
+	cout << endl;
 }
 
-int get_count_from_file(string file_name, string key) {
+bool match_criteria(int elem) {
 
-	string file_content = get_string_from_file(file_name);
-
+	if (elem % 3 == 0 && elem >0)
+		return true;
+	
+	return false;
+}
+int get_sum(int **arr, int rows, int colomn) {
+	int sum = 0;
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < colomn; j++)
+		{
+			if (match_criteria(arr[i][j]))
+				{
+					sum+= arr[i][j];
+				}
+		}
+	}
+	return sum;
+}
+int get_count(int **arr, int rows, int colomn) {
 	int count = 0;
-	for (int i = 0; i < key.length(); i++) {
-		for (int j = 0; j < file_content.length(); j++) {
-			if (file_content[j] == key[i])
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < colomn; j++)
+		{
+			if (match_criteria(arr[i][j]))
 			{
 				count++;
 			}
@@ -31,13 +64,84 @@ int get_count_from_file(string file_name, string key) {
 	}
 	return count;
 }
+
+void swap_colomn(int **arr, int rows, int column_1, int column_2) {
+	for (int i = 0; i < rows; i++) {
+		int tmp = arr[i][column_1];
+		arr[i][column_1] = arr[i][column_2];
+		arr[i][column_2] = tmp;
+	}
+
+}
+
+void bubbleSort(int **arr, int rows, int colomn)
+{
+	int i, j;
+	bool swapped;
+	for (i = 0; i < colomn - 1; i++) {
+		swapped = false;
+		for (j = 0; j < colomn - i - 1; j++) {
+			if (arr[0][j] < arr[0][j + 1]) {
+				swap_colomn(arr, rows, j, j + 1);
+				swapped = true;
+			}
+			if (arr[0][j] == arr[0][j + 1]) {
+				//cout << "They are equal" << j << endl;//go to 2 row
+				if (arr[1][j] < arr[1][j + 1]) {
+					swap_colomn(arr, rows, j, j + 1);
+				}
+				if (arr[1][j] == arr[1][j + 1]) {
+					if (arr[2][j] > arr[2][j + 1]) {
+						swap_colomn(arr, rows, j, j + 1);
+					}
+				}
+			}
+		}
+
+		// If no two elements were swapped
+		// by inner loop, then break
+		if (swapped == false)
+			break;
+	}
+}
+
+void change_matched_elem(int **arr, int rows, int colomn) {
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < colomn; j++)
+		{
+			if (match_criteria(arr[i][j]))
+			{
+				arr[i][j] = 0;
+			}
+		}
+	}
+ }
+
 int main() {
+	int **array;
+	array = new int *[9];
+	for (int i = 0; i < 9; i++)
+		array[i] = new int[6];
 
-	string key_word = "BASIC";
-	string file_name = "example.txt";
+	fill_array_with_randoms((int **)array, 9,6,-21,24);
+	array[0][3] = 17;
+	array[1][5] = 17;
+	cout << "Initial matrix: " << endl;
+	print_arr(array, 9, 6);
 
-	cout << "File is:\n" << get_string_from_file(file_name) << endl << endl;
-	cout << "Count =" << get_count_from_file(file_name, key_word) << endl;
+	bubbleSort(array, 9, 6);
+	cout << "Sorted matrix: " << endl;
+	print_arr(array, 9, 6);
+
+	int sum = get_sum(array, 9, 6);
+	int count = get_count(array, 9, 6);
+
+
+	change_matched_elem(array, 9, 6);
+	cout << "Modified matrix: " << endl;
+	print_arr(array, 9, 6);
+	cout << "Amount of elements =" << count << ", sum =" << sum << endl;
 
 	return 0;
 }
